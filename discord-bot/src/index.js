109f9bb95d243379ba0e595980dd5c2bce953aae
CorVous <60035620+CorVous@users.noreply.github.com>
2024-03-21 00:03:42 -0700

    I finally got audio conversion from discord working!!!!!!!!!

--- discord-bot/src/index.js
+++ discord-bot/src/index.js
@@ -1,27 +1,33 @@
-import Buffer from 'node:buffer';
-import process from 'node:process';
-import { URL } from 'node:url';
-import pkg from '@discordjs/opus';
-import { joinVoiceChannel } from '@discordjs/voice'
-import * as amqp from 'amqplib'
-import { Client, GatewayIntentBits, VoiceChannel, Events } from 'discord.js';
-import { loadCommands, loadEvents } from './util/loaders.js';
-import { registerEvents } from './util/registerEvents.js';
+import Buffer from "node:buffer";
+import process from "node:process";
+import { URL } from "node:url";
+import pkg from "@discordjs/opus";
+import { joinVoiceChannel } from "@discordjs/voice";
+import * as amqp from "amqplib";
+import { Client, Events, GatewayIntentBits, VoiceChannel } from "discord.js";
+import { loadCommands, loadEvents } from "./util/loaders.js";
+import { registerEvents } from "./util/registerEvents.js";
 
 const { OpusEncoder } = pkg;
-const encoder = new OpusEncoder(48_000, 2)
+const encoder = new OpusEncoder(48_000, 2);
 
 // Connect to rabbitmq
-const rabbitConnection = await amqp.connect('amqp://localhost')
-const rabbitChannel = await rabbitConnection.createChannel()
-const rabbitExchange = 'voice_buffer'
+const rabbitConnection = await amqp.connect("amqp://localhost");
+const rabbitChannel = await rabbitConnection.createChannel();
+const rabbitExchange = "voice_buffer";
 
 // Initialize the client
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
 
 // Load the events and commands
-const events = await loadEvents(new URL('events/', import.meta.url));
-const commands = await loadCommands(new URL('commands/', import.meta.url));
+const events = await loadEvents(new URL("events/", import.meta.url));
+const commands = await loadCommands(new URL("commands/", import.meta.url));
 
 // Register the event handlers
 registerEvents(commands, events, client);
@@ -32,8 +38,9 @@ void client.login(process.env.DISCORD_TOKEN);
 // listen for the client to be ready
 client.once(Events.ClientReady, async (cli) => {
   console.log(`Ready! Logged in as ${cli.user.tag}`);
-  const audioBuffers = {}
-  const channel = await client.channels.fetch('324917760578682881')
+  const audioBuffers = {};
+  const voiceSubs = {}
+  const channel = await client.channels.fetch("931844044680749076");
   if (channel instanceof VoiceChannel) {
     const connection = joinVoiceChannel({
       channelId: channel.id,
@@ -41,39 +48,39 @@ client.once(Events.ClientReady, async (cli) => {
       adapterCreator: channel.guild.voiceAdapterCreator,
       selfDeaf: false,
     });
-    console.log('Connected to channel ' + channel.name)
+    console.log("Connected to channel " + channel.name);
     connection.receiver.speaking.on("start", (uid) => {
-      const user = client.users.cache.get(uid)
-      console.log(user.username + ' is speaking')
-      const voiceSub = connection.receiver.subscribe(uid)
-      voiceSub.on("data", (chunk) => {
-        const audioChunk = encoder.decode(chunk)
+      const user = client.users.cache.get(uid);
+      console.log(user.username + " is speaking");
+      voiceSubs[uid] = connection.receiver.subscribe(uid);
+      voiceSubs[uid].on("data", (chunk) => {
+        const audioChunk = encoder.decode(chunk);
         if (Array.isArray(audioBuffers[uid])) {
-          audioBuffers[uid].push(audioChunk)
+          audioBuffers[uid].push(audioChunk);
         } else {
-          const newBuffer = []
-          newBuffer.push(audioChunk)
-          audioBuffers[uid] = newBuffer
+          const newBuffer = [];
+          newBuffer.push(audioChunk);
+          audioBuffers[uid] = newBuffer;
         }
-      })
-    })
-    connection.receiver.speaking.on("end", (uid) => {
-      const user = client.users.cache.get(uid)
-      const audioBuffer = Buffer.Buffer.concat(audioBuffers[uid])
-      const key = channel.guild.id + '.' + user.username
-      rabbitChannel.assertExchange(rabbitExchange, 'topic', {
-        durable: false
       });
-      rabbitChannel.publish(rabbitExchange, key, audioBuffer)
+    });
+    connection.receiver.speaking.on("end", (uid) => {
+      voiceSubs[uid].destroy()
+      const user = client.users.cache.get(uid);
+      const audioBuffer = Buffer.Buffer.concat(audioBuffers[uid]);
+      const key = channel.guild.id + "." + user.username;
+      rabbitChannel.assertExchange(rabbitExchange, "topic", {
+        durable: false,
+      });
+      rabbitChannel.publish(rabbitExchange, key, audioBuffer);
 
-      audioBuffers[uid] = []
-      console.log(user.username + ' has stopped speaking')
-    })
+      audioBuffers[uid] = [];
+      console.log(user.username + " has stopped speaking");
+    });
     setTimeout(() => {
-      connection.destroy()
-      rabbitConnection.close()
-      console.log('Disconnected from channel')
-    }, 600_000)
-
+      connection.destroy();
+      rabbitConnection.close();
+      console.log("Disconnected from channel");
+    }, 600_000);
   }
 });
--- faster-whisper/index.py
+++ faster-whisper/index.py
@@ -4,14 +4,22 @@ import pika
 import torch
 import io
 import numpy
+import librosa
+import samplerate
+import whisper
+from scipy.io.wavfile import write
+import scipy.signal as sps
+import audioop
 
-if False:
-    model_size = "large-v3"
+if torch.cuda.is_available():
+    model_size = "medium.en"
     model = WhisperModel(model_size, device="cuda", compute_type="float16")
+    w_model = whisper.load_model('medium.en')
     print("Cuda Available: ", model_size, "loaded...")
 else:
     model_size = "base.en"
     model = WhisperModel(model_size, device="cpu", compute_type="int8")
+    w_model = whisper.load_model('base.en')
     print("Cuda Unavailable: ", model_size, "loaded...")
 
 rabbit_connection = pika.BlockingConnection(
@@ -31,15 +39,32 @@ rabbit_channel.queue_bind(
 
 def callback(ch, method, properties, body):
     print(f" [x] {method.routing_key}")
-    audio_buffer = numpy.frombuffer(body, dtype=numpy.int16).astype(numpy.float32)
-    segments, _ = model.transcribe(audio_buffer)
-    segments = list(segments)  # The transcription will actually run here.
+    audio_buffer = audioop.ratecv(body, 2, 2, 48000, 16000, None)
+    audio_buffer = audioop.tomono(audio_buffer[0], 2, 1, 0)
+    aaaaa = numpy.frombuffer(audio_buffer, dtype=numpy.int16)
+    write('test348.wav', 16000, aaaaa)
+    # audio_np = numpy.frombuffer(body, dtype=numpy.int32).astype(numpy.int16)
+    # write('test2.wav', 48000, audio_np)
+    # audio_buffer = samplerate.resample(audio_np, 16000 * 1.0 / 48000, 'sinc_best')
+    # write('test.wav', 16000, audio_buffer)
+    # complete_audio = audio_buffer.astype(numpy.float32) / 32768.0
+    # num_samples = round(len(audio_np) * float(16000) / 48000)
+    # new_audio_np = sps.resample(audio_np, num_samples)
+    # write('test3.wav', 16000, new_audio_np)
+    complete_audio = aaaaa.astype(numpy.float32) / 32768.0
+    result = w_model.transcribe(complete_audio, fp16=torch.cuda.is_available())
+    print(f'Regular Whisper: {result['text']}')
+    segments, _ = model.transcribe(complete_audio)
+    segments = list(segments)
+    result = ""
     for segment in segments:
-        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+        result += segment.text + ' '
+    print("Faster Whisper: " + result)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
+rabbit_channel.basic_qos(prefetch_count=1)
 rabbit_channel.basic_consume(
     queue=rabbit_queue,
-    on_message_callback=callback,
-    auto_ack=True
+    on_message_callback=callback
 )
 rabbit_channel.start_consuming()
